c73673089c5f6ae2ae5668bb83fb8aaff7f42d6f
LancerComet <chw644@hotmail.com>
2023-05-05 18:26:05 +0800

    + Update build config.

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,6 +1,14 @@
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
+
 const baseConfig = require('./webpack.conf')
 
 baseConfig.mode = 'production'
 baseConfig.output.publicPath = '/Muso/'
+baseConfig.plugins.push(new CopyPlugin({
+  patterns: [
+    { from: path.resolve(__dirname, '../static'), to: path.resolve(__dirname, '../.demo') }
+  ]
+}))
 
 module.exports = baseConfig
